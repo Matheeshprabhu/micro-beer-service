0365545052e10b7466fc61eaa88fa4ac8c508d55
Matheesh <matheeshprabhu123@gmail.com>
2021-11-23 01:52:48 +0530

    Initial commit

---
+++ src/test/java/com/mat/microbeerservice/MicroBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mat.microbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
